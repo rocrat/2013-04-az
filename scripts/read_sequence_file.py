c8d9e57b92feb82ac1406800f77a8046e04144c0
rocrat <laroche_dominic@hotmail.com>
2013-04-05 15:09:39 -0700

    new script for reading sequences

---
+++ scripts/read_sequence_file.py
@@ -0,0 +1,18 @@
+import sys
+import screed
+
+filename = sys.argv[1]
+print filename
+
+#for record in screed.open(filename):
+#	print record
+#	break
+count = 0
+for record in screed.open(filename):
+	print record['sequence']
+	print count
+	count +=1
+	if count>=10:
+		break
+	
+	
